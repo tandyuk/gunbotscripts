8dd33bc516559e2ef649cf066c2e2409c8000281
James Tandy <admin@tandyukservers.co.uk>
2017-11-15 13:21:34 +0000

    Create bittrex-govenor.php

---
+++ bittrex-govenor.php
@@ -0,0 +1,27 @@
+<?PHP
+
+
+$basedir = dirname(__FILE__);
+include_once($basedir . '/bittrex-functions.php');
+global $config;
+$config = json_clean_decode(file_get_contents($basedir.'/config.js'),true);
+
+$orders = get_open_orders();
+$currencies = get_currencies();
+$markets = get_markets();
+
+
+var_dump($orders);
+
+if(count($orders)>0){
+foreach($orders as $key=>$order){
+
+if((time()-strtotime($order['Opened'])) > 300){
+echo "need to cancel order" . PHP_EOL;
+cancel_order($order['OrderUuid']);
+}
+
+}
+}
+
+?>

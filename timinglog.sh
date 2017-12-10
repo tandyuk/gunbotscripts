d6b5b04535b6d7130077b34fbca56441237b2dbd
James Tandy <admin@tandyukservers.co.uk>
2017-12-10 14:22:59 +0000

    Still work in progress...
    No longer requires you to pipe data to it, simply run "./timinglog.sh"
    You can also output to a logfile (eg output.csv) using "./timinglog.sh > output.csv"
    
    The output given is like this:
    
    ```App                           Pair                          Start                         End                           Delay                         Polltime                      Round
    b_BTC-ETH_ichimoku            BTC-ETH                       1512915334                    1512915335                    12                            1                             80
    b_BTC-BCC_stepgain            BTC-BCC                       1512915334                    1512915335                    12                            1                             80
    b_BTC-SALT_bbrsistepgain      BTC-SALT                      1512915335                    1512915336                    12                            1                             78
    b_BTC-EMC2_bbrsistepgain      BTC-EMC2                      1512915337                    1512915338                    12                            1                             79
    b_BTC-XEM_bbrsistepgain       BTC-XEM                       1512915340                    1512915341                    12                            1                             78
    b_BTC-ETH_ichimoku            BTC-ETH                       1512915334                    1512915342                    12                            8                             80
    b_BTC-XLM_bbrsistepgain       BTC-XLM                       1512915344                    1512915346                    11                            2                             80
    b_BTC-WAVES_bbrsistepgain     BTC-WAVES                     1512915344                    1512915346                    12                            2                             78
    b_BTC-ETH_ichimoku            BTC-ETH                       1512915346                    1512915347                    12                            1                             81
    b_BTC-BCC_stepgain            BTC-BCC                       1512915346                    1512915347                    12                            1                             81
    b_BTC-ETH_ichimoku            BTC-ETH                       1512915346                    1512915347                    12                            1                             81
    b_BTC-SALT_bbrsistepgain      BTC-SALT                      1512915347                    1512915348                    12                            1                             79
    b_BTC-NEO_emotionless         BTC-NEO                       1512915348                    1512915350                    15                            2                             72
    b_BTC-NEO_emotionless         BTC-NEO                       1512915348                    1512915350                    15                            2                             72
    b_BTC-EMC2_bbrsistepgain      BTC-EMC2                      1512915349                    1512915350                    12                            1                             80```
    
    This format is suitable for loading directly into excel for filtering, sorting and statistics on the pairs.
    Open the csv, and from the ribbon Data > Filter.
    I suggest filtering the pairname to ensure "Blanks" are shown, as these will include your total rows...
    Now scroll right to the end, and find the last row, we'll assume it is 457.
    In cells E and F 458 add  "=SUBTOTAL(101,E2:E457)" and "=SUBTOTAL(101,F2:F457)"
    You can now filter the results, and the average delay is shown in E458, and average poll duration in F458.
    
    Happy botting :)

--- timinglog.sh
+++ timinglog.sh
@@ -6,61 +6,124 @@ counter=0
 maxcounter=100  ## Set this to set how many rows to output at a time
 counterreset=1  ## Set this to 0 to echo headers again every $MAXCOUNTER lines, use anything greater than 0 for csv
 
-while read -r line
-do
-if [ "$counter" -eq "0" ]; then
-#output a heading row
-echo -e "App\tPair\tStart\tEnd\tDelay\tPolltime\tRound";
-#echo -e "1:App\t2:Pair\t3:Start\t4:End\t5:Delay\t6:Polltime\t7:Round";
-fi
+debug=0   #set this to 1 for some info, or 2 (or more) for a full stack trace
 
+tabs 30
 
-if [[ ${line} = *Round* ]]; then
-app=$(echo $line | jq .app_name | tr -d '"')
-#echo "APP: ${app}"
-msg=$(echo $line | jq .message | tr -d '"')
-#echo "MSG: ${msg}"
-sdate=$(echo $line | jq .timestamp | tr -d '"')
-stime=$(date -u -d $sdate +"%s")
-#echo "STARTTIME: ${stime}"
-pair=$(echo $msg | cut -d' ' -f4)
-#echo "PAIR: ${pair}"
-round=$(echo $msg | cut -d' ' -f7)
-#echo "ROUND: ${round}"
-lastpairs[$app]=$pair
-#echo "LASTPAIR: ${lastpairs[$app]}"
-pairstats[$app${pair}"_round"]=${round}
-pairstats[$app${pair}"_delay"]=$((stime-pairstats[${app}${pair}"_start"]))
-pairstats[$app${pair}"_start"]=${stime}
-fi
-
-##you could ananlyse other lines here....
-
-if [[ ${line} = *good* ]]; then
-app=$(echo $line | jq .app_name | tr -d '"')
-#echo "APP: ${app}"
-msg=$(echo $line | jq .message | tr -d '"')
-edate=$(echo $line | jq .timestamp | tr -d '"')
-etime=$(date -u -d $edate +"%s")
-#echo "ENDTIME: ${etime}"
-#[[ -n $app && -v $lastpairs[$app] ]] || break
-pair=${lastpairs[$app]}
-#echo "PAIR: ${pair}"
-pairstats[$app${pair}"_dur"]=$((etime-pairstats[${app}${pair}"_start"]))
-pairstats[$app${pair}"_end"]=${etime}
-
-
-#knowing this is the final bit, now we now echo out a complete row
+function outputlog () {
+app=$1
+pair=$2
 startkey=${app}${pair}"_start"
 endkey=${app}${pair}"_end"
 delaykey=${app}${pair}"_delay"
 durkey=${app}${pair}"_dur"
 roundkey=${app}${pair}"_round"
 
-echo -e "${app}\t${pair}\t${pairstats[${startkey}]}\t${pairstats[${endkey}]}\t${pairstats[${delaykey}]}\t${pairstats[${durkey}]}\t${pairstats[${roundkey}]}"
+if [[ "pairstats[${delaykey}]" -lt "1000" ]]; then
+if [[ "pairstats[${durkey}]" -lt "1000" ]]; then
+#if ((debug)); then
 #echo -e "1:${app}\t2:${pair}\t3:${pairstats[${startkey}]}\t4:${pairstats[${endkey}]}\t5:${pairstats[${delaykey}]}\t6:${pairstats[${durkey}]}\t7:${pairstats[${roundkey}]}"
+#else
+echo -e "${app}\t${pair}\t${pairstats[${startkey}]}\t${pairstats[${endkey}]}\t${pairstats[${delaykey}]}\t${pairstats[${durkey}]}\t${pairstats[${roundkey}]}"
+#fi
 #we also update out output row counter
 counter=$((counter+1))
+fi
+fi
+}
+
+function d () {
+if ((debug)); then echo $1 >&2; fi
+}
+if [[ debug -gt 1 ]]; then
+set -x
+fi
+
+
+
+
+while read -r line
+do
+if [ "$counter" -eq "0" ]; then
+#output a heading row
+#if ((debug)); then
+#echo -e "1:App\t2:Pair\t3:Start\t4:End\t5:Delay\t6:Polltime\t7:Round";
+#else
+echo -e "App\tPair\tStart\tEnd\tDelay\tPolltime\tRound";
+#fi
+counter=$((counter+1))
+fi
+
+
+if [[ ${line} = *Round* ]]; then
+app=$(echo $line | jq .app_name | tr -d '"')
+d "APP: ${app}"
+msg=$(echo $line | jq .message | tr -d '"')
+d "MSG: ${msg}"
+sdate=$(echo $line | jq .timestamp | tr -d '"')
+stime=$(date -u -d $sdate +"%s")
+d "STARTTIME: ${stime}"
+pair=$(echo $msg | cut -d' ' -f4)
+d "PAIR: ${pair}"
+round=$(echo $msg | cut -d' ' -f7)
+d "ROUND: ${round}"
+lastpairs[$app]=$pair
+d "LASTPAIR: ${lastpairs[$app]}"
+
+d "STATE: ${pairstats[$app${pair}_state]}"
+if (( pairstats[$app${pair}"_state"] )); then
+pairstats[$app${pair}"_dur"]=$((etime-pairstats[${app}${pair}"_start"]))
+pairstats[$app${pair}"_end"]=${etime}
+pairstats[$app${pair}"_state"]=0  #mark the row as complete
+#we also update out output row counter
+outputlog ${app} ${pair}
+#knowing this is the final bit of one type of log, now we now echo out a complete row
+fi
+
+pairstats[$app${pair}"_round"]=${round}
+pairstats[$app${pair}"_delay"]=$((stime-pairstats[${app}${pair}"_start"]))
+pairstats[$app${pair}"_start"]=${stime}
+pairstats[$app${pair}"_state"]=1  #mark pair as incomplete
+fi
+
+##you could ananlyse other lines here....
+
+
+
+if [[ ${line} = *recalc* ]]; then
+app=$(echo $line | jq .app_name | tr -d '"')
+d "APP: ${app}"
+msg=$(echo $line | jq .message | tr -d '"')
+edate=$(echo $line | jq .timestamp | tr -d '"')
+etime=$(date -u -d $edate +"%s")
+d "ENDTIME: ${etime}"
+#[[ -n $app && -v $lastpairs[$app] ]] || break
+pair=${lastpairs[$app]}
+d "PAIR: ${pair}"
+pairstats[$app${pair}"_dur"]=$((etime-pairstats[${app}${pair}"_start"]))
+pairstats[$app${pair}"_end"]=${etime}
+pairstats[$app${pair}"_state"]=0  #mark the row as complete
+#knowing this is the final bit of one type of log, now we now echo out a complete row
+outputlog ${app} ${pair}
+fi
+
+
+
+if [[ ${line} = *good* ]]; then
+app=$(echo $line | jq .app_name | tr -d '"')
+d "APP: ${app}"
+msg=$(echo $line | jq .message | tr -d '"')
+edate=$(echo $line | jq .timestamp | tr -d '"')
+etime=$(date -u -d $edate +"%s")
+d "ENDTIME: ${etime}"
+#[[ -n $app && -v $lastpairs[$app] ]] || break
+pair=${lastpairs[$app]}
+d "PAIR: ${pair}"
+pairstats[$app${pair}"_dur"]=$((etime-pairstats[${app}${pair}"_start"]))
+pairstats[$app${pair}"_end"]=${etime}
+pairstats[$app${pair}"_state"]=0  #mark the row as complete
+#knowing this is the final bit of one type of log, now we now echo out a complete row
+outputlog ${app} ${pair}
 fi
 
 
@@ -70,4 +133,4 @@ counter=$counterreset;
 fi
 
 
-done
+done< <(pm2 log --json | grep --line-buffered -iE 'good|round|recalc')
